9599e65b20f72c937a021f2a7f06956e9bb6f454
Gourav004 <gouravthakurpp@gmail.com>
2025-11-10 12:26:09 +0530

    fiexs

--- Frontend/src/Components/AdminViewProfile.jsx
+++ Frontend/src/Components/AdminViewProfile.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import useNavigate from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 
 function AdminViewProfile() {
